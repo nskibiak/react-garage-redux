0b2db5c9adb07141e43bfc52ec14b2644ce29745
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-24 15:19:19 +0900

    finished sidebar with image, logo, title, text and react-router Link

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Mechanic from '../assets/images/mechanic.jpg';
+import Logo from '../assets/images/logo.svg';
 
 class Sidebar extends Component {
   render () {
     return (
       <div className="aside">
-        "is this thing on?"
+        <div className="illustration">
+          <img src={Mechanic} alt="Mechanic" className="illustration"/>
+        </div>
+        <img src={Logo} alt="Logo" className="logo"/>
+        <h1>Garage Gaudin</h1>
+        <p>Our garage is the best. Reasonable prices, always on time, we are the best (and fictional).</p>
+        <Link to="/">Add a car</Link>
       </div>
     );
   }
