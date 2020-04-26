4140f884aacedbf9ef147025bd88c7ddd6c04cd8
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-26 22:27:42 +0900

    turned list container into separate container and rendered in app

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/stylesheets/application.scss';
 import Sidebar from './components/sidebar';
+import ListContainer from './containers/list_container';
 
 function App() {
   return(
     <div className="view-container">
       <Sidebar />
-      <div className="list-container"></div>
+      <ListContainer />
     </div>
   );
 }
---
+++ src/containers/list_container.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ListContainer extends Component {
+  render () {
+    return (
+      <div className="list-container"></div>
+    );
+  }
+}
+
+export default ListContainer;
