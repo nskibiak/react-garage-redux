cac62736f52a0330d520135a551afcda8fa13e29
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-29 18:27:21 +0900

    refactored car form into new page

--- src/containers/new_page.jsx
+++ src/containers/new_page.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { postCar } from '../actions';
 
 import SidebarOther from '../components/sidebar_other';
-import CarPage from '../containers/car_page';
+import CarForm from '../components/car_form';
+import formImage from '../assets/images/form.jpg';
 
 class NewPage extends Component {
+  submit = values => {
+      this.props.postCar(values);
+      this.props.history.push('/');
+  }
+
   render() {
     return(
       <div className="view-container">
         <SidebarOther />
-        <CarPage />
+        <div className="form-container">
+          <img className="overlay" src={formImage} alt="" />
+            <CarForm onSubmit={this.submit} />
+        </div>
       </div>
     );
   }
 }
 
-export default NewPage
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ postCar }, dispatch);
+}
 
+export default connect(null, mapDispatchToProps)(NewPage);
