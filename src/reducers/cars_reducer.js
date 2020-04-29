79c0ef0560520669c84cdb57d9a519a51a12c072
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-29 13:07:30 +0900

    updated reduer with POST CAR

--- src/reducers/cars_reducer.js
+++ src/reducers/cars_reducer.js
@@ -1,5 +1,5 @@
 import { FETCH_CARS } from '../actions';
-// import { POST_CAR } from '../actions';
+import { POST_CAR } from '../actions';
 
 export default function(state = [], action) {
   switch(action.type) {
@@ -7,7 +7,7 @@ export default function(state = [], action) {
       return state
     case FETCH_CARS:
       return action.payload
-    // case POST_CAR:
-    //   return [ action.payload ]
+    case POST_CAR:
+      return [ action.payload ]
   }
 }
