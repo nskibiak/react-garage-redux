784ae04ffbcc2bf0ff160e774da3b68d7fab8b45
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-29 21:20:03 +0900

    refactored IndexPage to remove ListContainer

--- src/containers/index_page.jsx
+++ src/containers/index_page.jsx
@@ -1,18 +1,45 @@
 import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { fetchCars } from '../actions';
+
+import Car from '../components/car';
 import SidebarIndex from '../components/sidebar_index';
-import ListContainer from '../containers/list_container';
+
 
 class IndexPage extends Component {
+  componentDidMount() {
+    this.props.fetchCars();
+  }
+
   render() {
     return(
       <Fragment>
         <SidebarIndex />
-        <ListContainer />
+        <div className="list-container">
+          {this.props.cars.map((car) => {
+            return(
+              <Link to={`/cars/${car.id}`} key={car.id}>
+                <Car key={car.id} car={car}/>
+              </Link>
+            );
+          })}
+        </div>
       </Fragment>
     );
   }
 }
 
-export default IndexPage
+function mapStateToProps(state) {
+  return {
+    cars: state.cars
+  };
+}
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchCars }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IndexPage);
